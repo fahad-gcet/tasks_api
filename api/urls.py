68f5b5800edd53ee3bc667d97d8e0442f1feb82d
fahad-gcet <fms525@outlook.com>
2017-09-19 14:51:15 +0530

    convert fbv to cbv

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url(r'^tasks/$', views.task_list, name='task_list'),
-    url(r'^tasks/(?P<pk>[0-9]+)$', views.task_detail, name='task_detail'),
+    url(r'^tasks/$', views.TaskList.as_view(), name='task_list'),
+    url(r'^tasks/(?P<pk>[0-9]+)$', views.TaskDetail.as_view(), name='task_detail'),
 ]
